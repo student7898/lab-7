87f1557bb4eba7e8dffa2b7cae0cb38b948ba416
student7898 <n01740597@humber.ca>
2025-04-03 17:19:12 -0400

    Changes background color on click

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 let button = document.getElementById("button");
+let body = document.body;
 
 function pressButton() {
     button.innerHTML = "Button Pressed! Try Double Clicking!";
+    body.style.backgroundColor = "lightblue";
 }
 
 function doubleClicked() {
     button.innerHTML = "Button Double Clicked!";
+    body.style.backgroundColor = "red";
 }
 
 function mouseEnter() {
     button.innerHTML = "Press Me!";
+    body.style.backgroundColor = "pink";
 }
 
 function mouseLeave() {
     button.innerHTML = "Hover Over Me!";
+    body.style.backgroundColor = "white";
 }
